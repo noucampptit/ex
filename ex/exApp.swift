5f9fe5e9889553d32fd271d7d52edbc229bbbc17
Vu Quoc Hung <vuquochung84@gmail.com>
2021-10-19 16:27:12 +0700

    Initial Commit

---
+++ ex/exApp.swift
@@ -0,0 +1,17 @@
+//
+//  exApp.swift
+//  ex
+//
+//  Created by Quoc Hung Vu on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
