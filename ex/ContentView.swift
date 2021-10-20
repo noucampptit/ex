bccd6dfd19263c36fb9e5d4e5365abe8410146ec
Vu Quoc Hung <vuquochung84@gmail.com>
2021-10-20 11:18:50 +0700

    dfdfd

--- ex/ContentView.swift
+++ ex/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!cdfdf")
             .padding()
-        Text("fdfdfdf")
+        Text("fdfdfdf dfdfdf")
+        
     }
 }
 
