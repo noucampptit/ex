0f34a40da5a17e07c77be3090228d44582e6f88f
Vu Quoc Hung <vuquochung84@gmail.com>
2021-10-20 15:01:12 +0700

    add file

---
+++ ex/View2.swift
@@ -0,0 +1,20 @@
+//
+//  View2.swift
+//  ex
+//
+//  Created by Quoc Hung Vu on 20/10/2021.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View2_Previews: PreviewProvider {
+    static var previews: some View {
+        View2()
+    }
+}
